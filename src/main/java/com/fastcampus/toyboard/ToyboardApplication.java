b563febe06a4467eb436a99213557806e2dabfe4
YongHo Shin <gdtknight@naver.com>
2023-07-07 23:31:32 +0900

    feat: project initialization

---
+++ src/main/java/com/fastcampus/toyboard/ToyboardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.toyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/toyboard/ToyboardApplicationTests.java
@@ -0,0 +1,11 @@
+package com.fastcampus.toyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyboardApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
