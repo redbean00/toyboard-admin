e1025f0fc8cfd6c3465734265feba01d9d7524d5
GyeongMin <aklmo3778@naver.com>
2024-02-02 13:52:49 +0900

    feat: 전체 메일 전송 기능 추가

--- src/main/java/com/fastcampus/toyboard/service/EmailService.java
+++ src/main/java/com/fastcampus/toyboard/service/EmailService.java
@@ -1,10 +1,15 @@
 package com.fastcampus.toyboard.service;
 
+import com.fastcampus.toyboard.dto.UserRoleDto;
 import com.fastcampus.toyboard.entity.Email;
+import com.fastcampus.toyboard.repository.UserRoleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -12,8 +17,19 @@ public class EmailService {
 
     private final JavaMailSender javaMailSender;
 
+    @Autowired
+    private final UserRoleRepository userRoleRepository;
+
+    SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+    public void sendMailAll(Email email){
+        List<UserRoleDto> userList = userRoleRepository.findAllWithAuthority();
+
+        for(UserRoleDto user : userList){
+            email.setTo(user.getEmail());
+            sendMail(email);
+        }
+    }
     public void sendMail(Email email) {
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
 
         simpleMailMessage.setTo(email.getTo());
         simpleMailMessage.setSubject(email.getSubject());
